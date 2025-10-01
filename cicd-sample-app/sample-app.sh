c2f1c4cc9b2d6abb0fabd7fe5605fd0d8725e753
renzdb <125889199+renzdb@users.noreply.github.com>
2025-10-01 12:19:39 +0200

    Update sample-app.sh

--- cicd-sample-app/sample-app.sh
+++ cicd-sample-app/sample-app.sh
@@ -5,7 +5,7 @@ mkdir -p tempdir
 mkdir -p tempdir/templates
 mkdir -p tempdir/static
 
-cp sample_app.py tempdir/.
+cp cicd-sample-app/sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
